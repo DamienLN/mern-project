670df67d4e4ac5d5d2b94222531ece349288663c
DamienLN <99394425+DamienLN@users.noreply.github.com>
2022-11-21 15:27:24 +0100

    auth 1.3 final

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
 const jwt = require("jsonwebtoken");
+const { signUpErrors, signInErrors } = require("../utils/errors.utils");
 
 const maxAge = 3 * 24 * 60 * 60 * 1000
 const createToken = (id) => {
@@ -9,7 +10,7 @@ const createToken = (id) => {
   })
 }
 
-// Inscription
+// registration
 module.exports.signUp = async (req, res) => {
   const { pseudo, email, password } = req.body;
 
@@ -17,7 +18,8 @@ module.exports.signUp = async (req, res) => {
     const user = await userModel.create({ pseudo, email, password });
     res.status(201).json({ user: user._id });
   } catch (err) {
-    res.status(200).json({ err });
+    const errors = signUpErrors(err);
+    res.status(200).send({errors});
   }
 };
 
@@ -33,7 +35,8 @@ module.exports.signIn = async (req, res) => {
     res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge})
     res.status(200).json({user: user._id})
   } catch (err) {
-    res.status(200).json(err)
+    const errors = signInErrors (err);
+    res.status(200).json({errors});
 
   }
 };
--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,2 +1,47 @@
 const jwt = requier('jsonwebtoken');
 const UserModel = require('../models/user.model')
+
+// test if user is connected
+module.exports.checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+    if (token){
+        // verify token
+        jwt.verify(token, proces.env.TOKEN_SECRET, async (err, decodedToken) => {
+            // error 
+            if (err) {
+                res.locals.user = null;
+                // delete the token in 1ms
+                res.cookie('jwt', '', {maxAge: 1});
+                next();
+            } else {
+                let user = await UserModel.findById(decodedToken.id)
+                res.locals.user = user;
+                console.log(user);
+                next();
+            } 
+        })
+    } else {
+        res.locals.user = null;
+        next();
+    }
+
+}
+
+module.exports.requireAuth = (req, res, next) => {
+    const token = req.cookies.jwt;
+    // if we have a token 
+    if (token) {
+        jwt.verify(token,process.env.TOKEN_SECRET, async (err, decodedToken) => {
+            if (err){
+                console.log(err);
+            } else {
+                console.log(decodedToken.id)
+                next();
+            }
+        })
+    }
+    // if we dont have a token
+    else {
+        console.log('No token');
+    }
+}
