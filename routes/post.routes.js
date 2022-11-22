d41bd8ea3aee694d0945417f75e711fdcce206ba
DamienLN <99394425+DamienLN@users.noreply.github.com>
2022-11-22 09:57:33 +0100

    Crud_Post_1.2

---
+++ routes/post.routes.js
@@ -0,0 +1,10 @@
+const router = rquire('express').Router();
+const postController = require('../controllers/psot.controller')
+
+router.get('/', postController.readPost);
+router.post('/', postController.createPost);
+router.put('/:id', postController.updatePost);
+router.delete('/:id', postController.deletePost);
+
+
+module.exports = router;
