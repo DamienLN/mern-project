699dd3fad6744cc688b5dcf68b8b53aa1546fcf3
DamienLN <99394425+DamienLN@users.noreply.github.com>
2022-11-24 14:01:59 +0100

    PB react-dom

---
+++ client/pages/Trending.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Trending = () => {
+  return (
+    <div>
+      hello depuis Trending
+    </div>
+  );
+};
+
+export default Trending;
---
+++ client/src/components/Routes/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router} from "react-router-dom";
+
+
+
+
+const index = () => {
+  return (
+    <Router>
+      <switch
+    </Router>
+  );
+};
+
+export default index;
