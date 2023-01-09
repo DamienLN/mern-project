06ba5ff4447925318f09d38d21b745da2834fb60
DamienLN <99394425+DamienLN@users.noreply.github.com>
2023-01-09 14:14:29 +0100

    front 1.1

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.j0m9jql.mongodb.net/mern-project"
+    "mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.j0m9jql.mongodb.net/Groupomania"
   )
 
   .then(() => console.log("Connected to mongoDB"))
