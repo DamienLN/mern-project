0ce78891f7e4ef4def6b0c45588ef92e33deeab8
DamienLN <99394425+DamienLN@users.noreply.github.com>
2023-01-09 16:00:42 +0100

    auth 1.1

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Log from '../components/Log';
 
-function Profil() {
+Const Profil = () => {
     return (
-        <div>
-            
+        <div className='profil-page'>
+            <div className='log-container'>
+                <Log> <Log/>
+                <div className="img-container">
+                    <img src='./img/log.svg' alt="img-log"/>
+                </div>
+            </div>
         </div>
     )
 }
