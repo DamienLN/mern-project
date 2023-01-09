107adf3a3366c4091a39f5621e00a53ebff47a2c
DamienLN <99394425+DamienLN@users.noreply.github.com>
2023-01-09 15:23:57 +0100

    1.4

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/index.scss';
 
+// Root qu'on retrouve dans public/index.html 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
